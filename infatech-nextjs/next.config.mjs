45915aeb341ce8ee35e8d69ff3e223a936d4d11e
Tabassum Sheliya <tabassumbanumusa@gmail.com>
2025-08-22 12:29:22 +0530

    pages

--- infatech-nextjs/next.config.mjs
+++ infatech-nextjs/next.config.mjs
@@ -6,11 +6,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:all*(svg|jpg|jpeg|png|gif|ico|webp|avif)",
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, s-maxage=86400, stale-while-revalidate=59',
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // 1 year cache
+          },
+        ],
+      },
+      {
+        source: "/_next/image",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, s-maxage=86400, stale-while-revalidate=59", // 1 day cache
           },
         ],
       },
