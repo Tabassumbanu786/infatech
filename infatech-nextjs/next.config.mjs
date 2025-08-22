bee3e18813a62ea1f1c7b363b685861a643375a2
Tabassum Sheliya <tabassumbanumusa@gmail.com>
2025-08-22 12:20:36 +0530

    pages

--- infatech-nextjs/next.config.mjs
+++ infatech-nextjs/next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.pexels.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['images.pexels.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=86400, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
