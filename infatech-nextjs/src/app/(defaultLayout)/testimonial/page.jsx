9ff1ea2778b0c0a6092ff3f7abdcd04a1dc9f1d4
Tabassum Sheliya <tabassumbanumusa@gmail.com>
2025-08-25 10:42:10 +0530

    blog and team

--- infatech-nextjs/src/app/(defaultLayout)/testimonial/page.jsx
+++ infatech-nextjs/src/app/(defaultLayout)/testimonial/page.jsx
@@ -1,17 +1,24 @@
 import Blog1 from '../../components/Blog/Blog1';
-
 import Testimonial4 from '../../components/Testimonial/Testimonial4';
-
 import React from 'react';
 
-const page = () => {
-    return (
-        <div className='testimonial-page'>
-            {/* <BreadCumb Title="Testimonial"></BreadCumb> */}
-            <Testimonial4></Testimonial4>
-            <Blog1></Blog1>
-        </div>
-    );
+// 👇 Yeh metadata Next.js App Router ke liye hai
+export const metadata = {
+  title: "Testimonial Page",
+  robots: {
+    index: false,   // noindex
+    follow: false,  // nofollow
+  },
 };
 
-export default page;
+const Page = () => {
+  return (
+    <div className='testimonial-page'>
+      {/* <BreadCumb Title="Testimonial"></BreadCumb> */}
+      <Testimonial4 />
+      <Blog1 />
+    </div>
+  );
+};
+
+export default Page;
